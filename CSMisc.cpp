33cbbf8a618c3edceb49ed2a1fff7fe5acb2d74c
Tyler Tepper <157242593+ttepper@users.noreply.github.com>
2024-09-04 12:46:49 -0500

    Add files via upload

---
+++ CSMisc.cpp
@@ -0,0 +1,11 @@
+#include "CSMisc.h"
+#include <iostream>
+using namespace std;
+
+void CSMisc::getChars()
+{
+	char c = ' ';
+	cout << "Enter some characrers: ";
+	cin >> c;
+	cout << "Character entered: " << c endl;
+}
---
+++ CSMisc.h
@@ -0,0 +1,10 @@
+#pragma once
+class CSMisc
+{
+private:
+
+public:
+	void getChars();
+	void getEntireLine();
+	void getWord(); // word means until a space character
+};
